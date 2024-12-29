d4ad898d55c223fb6615b272ea8951d978b09ad3
MuhSaktiR <seken876@gmail.com>
2024-12-29 13:49:48 +0700

    Memperbaiki typo pada repository dan menghapus response void

--- app/src/main/java/com/example/activityremotedatabase/repository/MahasiswaRepository.kt
+++ app/src/main/java/com/example/activityremotedatabase/repository/MahasiswaRepository.kt
@@ -15,33 +15,34 @@ interface MahasiswaRepository {
 
 class NetworkMahasiswaRepository(
     private val mahasiswaApiService: MahasiswaService
-) : MahasiswaService {
-    override suspend fun insertMahasiswa(mahasiwa: Mahasiswa) {
-        mahasiswaApiService.insertMahasiswa(mahasiwa)
+) : MahasiswaRepository{
+
+    override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
+        mahasiswaApiService.insertMahasiswa(mahasiswa)
     }
 
-    override suspend fun updateMahasiswa(nim: String, mahasiwa: Mahasiswa) {
-        mahasiswaApiService.updateMahasiswa(nim, mahasiwa)
+    override suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa) {
+        mahasiswaApiService.updateMahasiswa(nim, mahasiswa)
     }
 
-    override suspend fun deleteMahasiswa(nim: String): Response<Void> {
+    override suspend fun deleteMahasiswa(nim: String) {
         try {
             val response = mahasiswaApiService.deleteMahasiswa(nim)
             if (!response.isSuccessful) {
-                throw IOException ("Failed to delete mahasiswa. HTTP Status code: " +
-                    "${response.code()}")
+                throw IOException("Failed to delete mahasiswa. HTTP Status Code: " +
+                        "${response.code()}")
             } else {
                 response.message()
                 println(response.message())
             }
-        } catch (e: Exception) {
+        } catch (e:Exception){
             throw e
         }
     }
 
-    override suspend fun getMahasiswa(): List<Mahasiswa> = mahasiswaApiService. getMahasiswa()
+    override suspend fun getMahasiswa(): List<Mahasiswa> = mahasiswaApiService.getMahasiswa()
 
     override suspend fun getMahasiswaByNim(nim: String): Mahasiswa {
-        return mahasiswaApiService.getMahasiswaByNim(nim)
+        return  mahasiswaApiService.getMahasiswaByNim(nim)
     }
 }
