7e77b6870704a968cbdd688af66c79f035e5b513
MuhSaktiR <seken876@gmail.com>
2024-12-29 18:30:15 +0700

    Membuat navigasi antar halaman HomeScreen dan EntryMhsScreen dengan pengelolaan route menggunakan NavHost

---
+++ app/src/main/java/com/example/activityremotedatabase/ui/navigation/PengelolaHalaman.kt
@@ -0,0 +1,48 @@
+package com.example.activityremotedatabase.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.activityremotedatabase.ui.view.DestinasiEntry
+import com.example.activityremotedatabase.ui.view.DestinasiHome
+import com.example.activityremotedatabase.ui.view.EntryMhsScreen
+import com.example.activityremotedatabase.ui.view.HomeScreen
+
+@Composable
+fun PengelolaHalaman(
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(
+            DestinasiHome.route
+        ) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route)},
+                onDetailClick = {
+
+                }
+            )
+        }
+
+        composable(
+            DestinasiEntry.route
+        ) {
+            EntryMhsScreen(
+                navigateBack = {
+                    navController.navigate(DestinasiHome.route){
+                        popUpTo(DestinasiHome.route) {
+                            inclusive = true
+                        }
+                    }
+                })
+        }
+    }
+}
