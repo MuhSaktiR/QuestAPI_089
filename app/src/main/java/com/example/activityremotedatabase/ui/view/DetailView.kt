2c5f3c17c1f6fe13c2f15265457de7dd613cf9f9
MuhSaktiR <seken876@gmail.com>
2024-12-30 10:45:42 +0700

    Membuat DetailScreen untuk menampilkan detail mahasiswa dengan navigasi, top app bar, dan tombol aksi untuk edit

--- app/src/main/java/com/example/activityremotedatabase/ui/view/DetailView.kt
+++ app/src/main/java/com/example/activityremotedatabase/ui/view/DetailView.kt
@@ -5,9 +5,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -16,8 +22,60 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.activityremotedatabase.model.Mahasiswa
+import com.example.activityremotedatabase.ui.customwidget.CostumeTopAppBar
+import com.example.activityremotedatabase.ui.navigation.DestinasiNavigasi
 import com.example.activityremotedatabase.ui.viewmodel.DetailUiState
+import com.example.activityremotedatabase.ui.viewmodel.DetailViewModel
+import com.example.activityremotedatabase.ui.viewmodel.PenyediaViewModel
+
+object DestinasiDetail: DestinasiNavigasi{
+    override val route = "detail"
+    override val titleRes = "Detail Mhs"
+    const val NIM = "nim"
+    val routesWithArg = "$route/{$NIM}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailScreen(
+    navigateBack: () -> Unit,
+    onEditClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    Scaffold(
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiDetail.titleRes,
+                canNavigateBack = true,
+                navigateUp = navigateBack,
+                onRefresh = {
+                    viewModel.getMahasiswabyNim()
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onEditClick,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Edit Mahasiswa"
+                )
+            }
+        }
+    ) { innerPadding ->
+        DetailStatus(
+            modifier = Modifier.padding(innerPadding),
+            detailUiState = viewModel.detailUiState,
+            retryAction = { viewModel.getMahasiswabyNim() }
+        )
+    }
+}
 
 @Composable
 fun DetailStatus(
