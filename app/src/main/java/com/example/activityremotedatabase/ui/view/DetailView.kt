9a34f1bcba7b43fd759765311f73c9e618c3f06c
MuhSaktiR <seken876@gmail.com>
2024-12-30 09:49:25 +0700

    Membuat fungsi ItemDetailMhs dan DetailMhs untuk menampilkan detail mahasiswa

---
+++ app/src/main/java/com/example/activityremotedatabase/ui/view/DetailView.kt
@@ -0,0 +1,63 @@
+package com.example.activityremotedatabase.ui.view
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.activityremotedatabase.model.Mahasiswa
+
+@Composable
+fun ItemDetailMhs(
+    modifier: Modifier = Modifier,
+    mahasiswa: Mahasiswa
+) {
+    Card(
+        modifier = modifier.padding(16.dp),
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            DetailMhs(judul = "NIM", isinya = mahasiswa.nim)
+            DetailMhs(judul = "Nama", isinya = mahasiswa.nama)
+            DetailMhs(judul = "Alamat", isinya = mahasiswa.alamat)
+            DetailMhs(judul = "Jenis Kelamin", isinya = mahasiswa.jenisKelamin)
+            DetailMhs(judul = "Kelas", isinya = mahasiswa.kelas)
+            DetailMhs(judul = "Angkatan", isinya = mahasiswa.angkatan)
+        }
+    }
+}
+
+@Composable
+fun DetailMhs(
+    modifier: Modifier =Modifier,
+    judul: String,
+    isinya: String,
+){
+    Column(modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start)
+    {
+        Text(
+            text = "$judul : ",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+        Text(
+            text = isinya,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
