d1a1fd3fe403b25ec289dba0568f47067878312c
MuhSaktiR <seken876@gmail.com>
2024-12-30 11:51:11 +0700

    Menambahkan inisialisasi untuk DetailViewModel dan UpdateViewModel dengan menggunakan SavedStateHandle

--- app/src/main/java/com/example/activityremotedatabase/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/activityremotedatabase/ui/viewmodel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.activityremotedatabase.ui.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -10,6 +11,8 @@ object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomeViewModel(aplikasiMhs().container.mahasiswaRepository) }
         initializer { InsertViewModel(aplikasiMhs().container.mahasiswaRepository) }
+        initializer { DetailViewModel(createSavedStateHandle(),aplikasiMhs().container.mahasiswaRepository)}
+        initializer { UpdateViewModel(createSavedStateHandle(),aplikasiMhs().container.mahasiswaRepository)}
     }
 }
 
